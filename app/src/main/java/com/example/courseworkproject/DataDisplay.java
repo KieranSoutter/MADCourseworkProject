55317fccf1f9de52274b60f2be4c3b12e136b240
KieranSoutter <72031734+KieranSoutter@users.noreply.github.com>
2020-11-18 17:07:43 +0000

    Changelog Train App 0.2:
    -Added both Main and DataDisplay Activity's
    -Various UI Changes
    -Buttons now function
    -Data link set between Activtiy's
    
    Kieran Soutter
    18/11/2020

---
+++ app/src/main/java/com/example/courseworkproject/DataDisplay.java
@@ -0,0 +1,47 @@
+package com.example.courseworkproject;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataDisplay extends AppCompatActivity implements View.OnClickListener {
+
+    public String stationName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+
+        Intent laucher = getIntent();
+        if (laucher.hasExtra(MainActivity.EXTRA_DISPLAYEDSTATION)){
+            TextView tvStationName = findViewById(R.id.tvStationName);
+            String displaytext = laucher.getStringExtra(MainActivity.EXTRA_DISPLAYEDSTATION) + " Station Departures";
+            tvStationName.setText(displaytext);
+        }
+
+
+        // Sets up the buttons with the onClickListener() function
+        Button btnGetStationMain = findViewById(R.id.btnGetStationData);
+        //This didn't work the first time, this didn't work the second time. I DON'T KNOW WHY ON EARTH THIS WORKED THE 3RD TIME... Oh Well!
+        btnGetStationMain.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //If the button clicked is the station Search button then...
+        if (view.getId() == R.id.btnGetStationData){
+            //... get the location entered by the user...
+            EditText etLocation = findViewById(R.id.etGetLocation);
+            stationName = String.valueOf(etLocation.getText());
+        }
+    }
+}
--- app/src/main/java/com/example/courseworkproject/MainActivity.java
+++ app/src/main/java/com/example/courseworkproject/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.courseworkproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String displayedStation;
+
+    public static final String EXTRA_DISPLAYEDSTATION = "Station";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sets up the buttons with the onClickListener() function
+        Button btnGetStationMain = findViewById(R.id.btn_StationSearch);
+            //This didn't work the first time, this didn't work the second time. I DON'T KNOW WHY ON EARTH THIS WORKED THE 3RD TIME... Oh Well!
+        btnGetStationMain.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View view) {
+        //If the button clicked is the station Search button then...
+        if (view.getId() == R.id.btn_StationSearch){
+            //... get the location entered by the user...
+            EditText etLocation = findViewById(R.id.etGetLocation);
+            displayedStation = String.valueOf(etLocation.getText());
+            displayDataDisplayActivity();
+        }
+
+
+    }
+
+    private void displayDataDisplayActivity(){
+        Intent intent = new Intent(getApplicationContext(), DataDisplay.class);
+
+        //add the station name
+        intent.putExtra(EXTRA_DISPLAYEDSTATION, displayedStation);
+
+        //start the activity
+        startActivity(intent);
+
+    }
+
 }

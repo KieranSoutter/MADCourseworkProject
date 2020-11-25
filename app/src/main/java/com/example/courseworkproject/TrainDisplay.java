0a0719c5b56ca9bb51394c5604576aa483a6ca19
KieranSoutter <72031734+KieranSoutter@users.noreply.github.com>
2020-11-25 17:51:15 +0000

    Changelog Train App 0.5:
    -API Station Name > Station Code working
    -Reworked How train info methods will function together
    
    Kieran Soutter
    25/11/2020

--- app/src/main/java/com/example/courseworkproject/TrainDisplay.java
+++ app/src/main/java/com/example/courseworkproject/TrainDisplay.java
@@ -10,19 +10,28 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
 import static android.content.ContentValues.TAG;
 
 public class TrainDisplay {
 
-    public static void getTrainInfoFromCloud(Context context, String station){
+
+    public static String getStationInfoFromCloud(Context context, String station){
         String applicationID = "d8e690f5";
         String apiKey = "a64dcf1d1dd8d45481bd33c43bafe55a";
         String url = "http://transportapi.com/v3/uk/places.json?query=" + station + "&type=train_station&app_id=" + applicationID +"&app_key=" + apiKey;
+
+        final String[] suffix = new String[1];
 
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.d(TAG, "Success" + response);
+                suffix[0] = getSuffix(response);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -33,5 +42,28 @@ public class TrainDisplay {
 
         RequestQueue requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         requestQueue.add(request);
+
+        return suffix[0];
+    }
+
+    public static String getSuffix(String jsonString){
+        String suffix;
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray suffixObject = jsonObject.getJSONArray("member");
+            JSONObject arrayObject = suffixObject.getJSONObject(0);
+            suffix = arrayObject.getString("station_code");
+            Log.d(TAG, "Success" + suffix);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            suffix = "error";
+        }
+        return suffix;
+    }
+
+    public static void getTrainInfoFromCloud(Context context, String station){
+
+        String suffix = getStationInfoFromCloud(context, station);
+
     }
 }
